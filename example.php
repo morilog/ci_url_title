194b98a86d72682e785ec0270a96d388bf71bb82
Morteza <m.parvini@outlook.com>
2014-01-21 11:21:14 +0330

    example of usage

---
+++ example.php
@@ -0,0 +1,12 @@
+﻿<?php
+
+class ExampleController extends Controller{
+	function index()
+	{
+		$this->load->helper('url');
+		$title = 'این یک عنوان نمونه است';
+		
+		echo url_title($title);//این-یک-عنوان-نمونه-است
+	}
+
+}
